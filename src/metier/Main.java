31c50b438017e4d6c5ae24a29a38e1632820c355
Mohamed El azzouzi <mohamed.el-azzouzi@etu.univ-lyon1.fr>
2019-06-17 00:48:46 +0200

    ajout luminosite et nettoyage

--- src/metier/Main.java
+++ src/metier/Main.java
@@ -1,9 +1,45 @@
 package metier;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import application.FenetreATM;
 
 public class Main {
 	public static void main(String[] args) {
+		new Thread(() -> {
+			String[] command =
+			    {
+			        "cmd",
+			    };
+			    Process p = null;
+				try {
+					p = Runtime.getRuntime().exec(command);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			    new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+			    new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+			    PrintWriter stdin = new PrintWriter(p.getOutputStream());
+			    
+			    stdin.println("cd runnable");
+			    for (int i=0; i!=-1; i++) {
+				    Integer btni = 20*i%100;
+				    String btn = btni.toString();
+				    stdin.println("nircmd.exe setbrightness " + btn);
+				    stdin.flush();
+				    //wait some time
+				    try {
+				    	Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				    
+			    }
+			    stdin.close();
+		}).start();
+		
 		new FenetreATM();
 	}
 }
